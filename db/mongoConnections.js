2c0dedc9ec9a737c0077cbc6afec6e1e9abfdbc7
prasantip <prasanti.prusty@rapidfunnel.com>
2023-02-21 20:26:05 +0530

    Remove connectTimeoutMS from the mongoos connect param

--- db/mongoConnections.js
+++ db/mongoConnections.js
@@ -2,12 +2,12 @@ import mongoose from 'mongoose'
 
 export function connect() {
     mongoose.set('strictQuery', false);
-    const url = 'mongodb+srv://' + process.env.MONGODB_USERNAME + ':' + process.env.MONGODB_PASSWORD
+    const uri = 'mongodb+srv://' + process.env.MONGODB_USERNAME + ':' + process.env.MONGODB_PASSWORD
         + '@' + process.env.MONGODB_CLUSTER + '/' + process.env.MONGODB_DB + '?retryWrites=true&w=majority'
 
     // To handle initial connection errorscusing promises
-    mongoose.connect(url,
-        { useNewURLParser: true, useUnifiedTopology: true, connectTimeoutMS: 100 })
+    mongoose.connect(uri,
+        { useNewURLParser: true, useUnifiedTopology: true })
         .then(() => {
             /** ready to use. The `mongoose.connect()` promise resolves to mongoose instance. */
             console.log('Successfully connected to MongoDB Atlas!')
