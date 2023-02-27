4d227e69596a3044ec704cfede6c018598f6caaf
prasantip <prasanti.prusty@rapidfunnel.com>
2023-02-27 11:20:43 +0530

    Refactored the views

--- src/controllers/event.js
+++ src/controllers/event.js
@@ -74,11 +74,11 @@ const eventController = {
     }
 }
 
-function wrapAsync(fn) {
-    return function (req, res, next) {
-        fn(req, res, next).catch(e => next(e))
-    }
+// function wrapAsync(fn) {
+//     return function (req, res, next) {
+//         fn(req, res, next).catch(e => next(e))
+//     }
 
-}
+// }
 
 export default eventController
